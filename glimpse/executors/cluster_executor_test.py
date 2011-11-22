3096ae637f35b2919d4106b2b3d3f6fcd93f3a66
Mick Thomure <mthomure@gmail.com>
2011-11-21 22:44:33 -0800

    Added tests for multiprocessing (as opposed to multi-threaded) cluster executors.

--- glimpse/executors/cluster_executor_test.py
+++ glimpse/executors/cluster_executor_test.py
@@ -4,18 +4,136 @@ from glimpse.executors.executor import StaticMap
 from glimpse.util.zmq_cluster import Connect, Worker
 import logging
 import threading
+import multiprocessing
 import time
 import unittest
 import zmq
 
-def WorkerTarget(context, request_receiver, result_sender, callback,
+def WorkerProcessTarget(request_receiver, result_sender, callback,
+    command_receiver):
+  context = zmq.Context()
+  worker = Worker(context, request_receiver, result_sender, callback,
+      command_receiver)
+  worker.Setup()
+  worker.Run()
+
+class TestClusterExecutor_Multicore(unittest.TestCase):
+
+  def test_quitsOnCommand(self):
+    use_threading = False
+    callback = lambda x: x * 100
+    context = zmq.Context()
+    # bind the command_sender socket
+    command_sender = Connect(url = "ipc://commands.ipc", bind = True)
+    # launch the cluster executor
+    #   request_sender and result_receiver bind
+    #   command_receiver connects
+    request_sender = Connect("ipc://requests.ipc", bind = True)
+    result_receiver = Connect("ipc://results.ipc", bind = True)
+    command_receiver = Connect("ipc://commands.ipc",
+        options = {zmq.SUBSCRIBE : ""})  # subscribe to all command messages
+    executor = ClusterExecutor(context, request_sender, result_receiver,
+        command_sender, command_receiver, use_threading = use_threading)
+    executor.Setup()
+    time.sleep(1)
+    # launch the worker
+    #   all sockets connect
+    request_receiver = Connect("ipc://requests.ipc")
+    result_sender = Connect("ipc://results.ipc")
+    worker = multiprocessing.Process(target = WorkerProcessTarget, args = (
+        request_receiver, result_sender, callback, command_receiver))
+    worker.daemon = True
+    worker.start()
+    # tear down the worker
+    Worker.SendKillCommand(context, command_sender)
+    worker.join(100)
+    self.assertFalse(worker.is_alive())
+    # tear down the cluster executor
+    executor.Shutdown()
+    self.assertFalse(executor.sink.is_alive())
+
+  # test that basic Put()/Get() call pair works
+  def test_putGet(self):
+    use_threading = False
+    callback = lambda x: x * 100
+    context = zmq.Context()
+    # bind the command_sender socket
+    command_sender = Connect(url = "ipc://commands.ipc", bind = True)
+    # launch the cluster executor
+    #   request_sender and result_receiver bind
+    #   command_receiver connects
+    request_sender = Connect("ipc://requests.ipc", bind = True)
+    result_receiver = Connect("ipc://results.ipc", bind = True)
+    command_receiver = Connect("ipc://commands.ipc",
+        options = {zmq.SUBSCRIBE : ""})  # subscribe to all command messages
+    executor = ClusterExecutor(context, request_sender, result_receiver,
+        command_sender, command_receiver, use_threading = use_threading)
+    executor.Setup()
+    time.sleep(1)
+    # launch the worker
+    #   all sockets connect
+    request_receiver = Connect("ipc://requests.ipc")
+    result_sender = Connect("ipc://results.ipc")
+    worker = multiprocessing.Process(target = WorkerProcessTarget, args = (
+        request_receiver, result_sender, callback, command_receiver))
+    worker.daemon = True
+    worker.start()
+    executor.Put(2)
+    self.assertEqual(200, executor.Get())
+    # tear down the worker
+    Worker.SendKillCommand(context, command_sender)
+    worker.join(100)
+    self.assertFalse(worker.is_alive())
+    # tear down the cluster executor
+    executor.Shutdown()
+    self.assertFalse(executor.sink.is_alive())
+
+  # test that map works
+  def test_map(self):
+    use_threading = True
+    callback = lambda x: x * 100
+    context = zmq.Context()
+    # bind the command_sender socket
+    command_sender = Connect(url = "ipc://commands.ipc", bind = True)
+    # launch the cluster executor
+    #   request_sender and result_receiver bind
+    #   command_receiver connects
+    request_sender = Connect("ipc://requests.ipc", bind = True)
+    result_receiver = Connect("ipc://results.ipc", bind = True)
+    command_receiver = Connect("ipc://commands.ipc",
+        options = {zmq.SUBSCRIBE : ""})  # subscribe to all command messages
+    executor = ClusterExecutor(context, request_sender, result_receiver,
+        command_sender, command_receiver, use_threading = use_threading)
+    executor.Setup()
+    time.sleep(1)
+    # launch the worker
+    #   all sockets connect
+    request_receiver = Connect("ipc://requests.ipc")
+    result_sender = Connect("ipc://results.ipc")
+    worker = multiprocessing.Process(target = WorkerProcessTarget, args = (
+        request_receiver, result_sender, callback, command_receiver))
+    worker.daemon = True
+    worker.start()
+    xs = range(10)
+    expected = map(callback, xs)
+    actual = sorted(StaticMap(executor, xs))
+    self.assertEqual(expected, actual)
+    # tear down the worker
+    Worker.SendKillCommand(context, command_sender)
+    worker.join(100)
+    self.assertFalse(worker.is_alive())
+    # tear down the cluster executor
+    executor.Shutdown()
+    self.assertFalse(executor.sink.is_alive())
+
+def WorkerThreadTarget(context, request_receiver, result_sender, callback,
     command_receiver):
   worker = Worker(context, request_receiver, result_sender, callback,
       command_receiver)
   worker.Setup()
   worker.Run()
 
-class TestClusterExecutor(unittest.TestCase):
+class TestClusterExecutor_Threaded(unittest.TestCase):
 
   def test_quitsOnCommand(self):
     use_threading = True
@@ -39,7 +157,7 @@ class TestClusterExecutor(unittest.TestCase):
     #   all sockets connect
     request_receiver = Connect("inproc://requests")
     result_sender = Connect("inproc://results")
-    worker = threading.Thread(target = WorkerTarget, args = (context,
+    worker = threading.Thread(target = WorkerThreadTarget, args = (context,
         request_receiver, result_sender, callback, command_receiver))
     worker.daemon = True
     worker.start()
@@ -74,7 +192,7 @@ class TestClusterExecutor(unittest.TestCase):
     #   all sockets connect
     request_receiver = Connect("inproc://requests")
     result_sender = Connect("inproc://results")
-    worker = threading.Thread(target = WorkerTarget, args = (context,
+    worker = threading.Thread(target = WorkerThreadTarget, args = (context,
         request_receiver, result_sender, callback, command_receiver))
     worker.daemon = True
     worker.start()
@@ -111,7 +229,7 @@ class TestClusterExecutor(unittest.TestCase):
     #   all sockets connect
     request_receiver = Connect("inproc://requests")
     result_sender = Connect("inproc://results")
-    worker = threading.Thread(target = WorkerTarget, args = (context,
+    worker = threading.Thread(target = WorkerThreadTarget, args = (context,
         request_receiver, result_sender, callback, command_receiver))
     worker.daemon = True
     worker.start()
