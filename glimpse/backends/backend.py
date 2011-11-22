015eec6e255a8224cf2953a8690fb00f9c0d3378
Mick Thomure <mthomure@gmail.com>
2011-11-21 22:49:34 -0800

    Added interface definition (i.e., documentation) for backends.

---
+++ glimpse/backends/backend.py
@@ -0,0 +1,89 @@
+# Copyright (c) 2011 Mick Thomure
+# All rights reserved.
+#
+# Please see the file COPYING in this distribution for usage terms.
+
+# Interface for filter operations.
+
+class IBackend(object):
+
+  def ContrastEnhance(self, data, kwidth, bias, scaling):
+    """Apply local contrast stretch to an array.
+    data -- (2-D) array of input data
+    kwidth -- (int) kernel width
+    bias -- (float) additive term in denominator
+    scaling -- (positive int) subsampling factor
+    """
+
+  def DotProduct(self, data, kernels, scaling):
+    """Convolve an array with a set of kernels.
+    data -- (3-D) array of input data
+    kernels -- (4-D) array of (3-D) kernels
+    scaling -- (positive int) subsampling factor
+    """
+
+  def NormDotProduct(self, data, kernels, bias, scaling):
+    """Convolve an array with a set of kernels, normalizing the response by the
+    vector length of the input neighborhood.
+    data -- (3-D) array of input data
+    kernels -- (4-D) array of (3-D) kernels, where each kernel is expected to
+              have unit vector length
+    bias -- (float) additive term in denominator
+    scaling -- (positive int) subsampling factor
+    """
+
+  def Rbf(self, data, kernels, beta, scaling):
+    """Compare kernels to input data using the RBF activation function.
+    data -- (3-D) array of input data
+    kernels -- (4-D) array of (3-D) kernels
+    beta -- (positive float) tuning parameter for radial basis function
+    scaling -- (positive int) subsampling factor
+    """
+
+  def NormRbf(self, data, kernels, bias, beta, scaling):
+    """Compare kernels to input data using the RBF activation function with
+       normed inputs.
+    data -- (3-D) array of input data
+    kernels -- (4-D) array of (3-D) kernels, where each kernel is expected to
+        have unit length
+    bias -- (float) additive term in denominator
+    beta -- (positive float) tuning parameter for radial basis function
+    scaling -- (positive int) subsampling factor
+    """
+
+  def LocalMax(self, data, kwidth, scaling):
+    """Convolve maps with local 2-D max filter.
+    data -- (3-D) array of input data
+    kwidth -- (positive int) kernel width
+    scaling -- (positive int) subsampling factor
+    """
+
+  def GlobalMax(self, data):
+    """Find the per-band maxima.
+    data -- (3-D) array of input data
+    """
+
+  def OutputMapShapeForInput(self, kheight, kwidth, scaling, iheight, iwidth):
+    """Given an input map with the given dimensions, compute the shape of the
+    corresponding output map.
+    kheight -- (positive int) kernel height
+    kwidth -- (positive int) kernel width
+    scaling -- (positive int) subsampling factor
+    iheight -- (positive int) input map height
+    iwidth -- (positive int) input map width
+    RETURNS (tuple) output map height and width, in that order
+    """
+
+  def InputMapShapeForOutput(self, kheight, kwidth, scaling, oheight, owidth):
+    """The inverse of OutputMapShapeForInput(). Given an output map with the
+    given dimensions, compute the shape of the corresponding input map.
+    kheight -- (positive int) kernel height
+    kwidth -- (positive int) kernel width
+    scaling -- (positive int) subsampling factor
+    oheight -- (positive int) output map height
+    owidth -- (positive int) output map width
+    RETURNS (tuple) input map height and width, in that order
+    """
+
+  def PrepareArray(self, array):
+    """Prepare array to be passed to backend methods."""
