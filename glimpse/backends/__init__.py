5a442332a58fca3fa000400a5bfba994ff35bd6f
Mick Thomure <mthomure@gmail.com>
2011-12-01 17:08:57 -0800

    Added helper method to create default backend.

--- glimpse/backends/__init__.py
+++ glimpse/backends/__init__.py
@@ -6,3 +6,7 @@
 
 from cython_backend import CythonBackend
 from scipy_backend import ScipyBackend
+
+def ChooseBackend():
+  """Return an instance for the best available backend."""
+  return CythonBackend()
