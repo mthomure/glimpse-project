5e75fc92cb50c757b1fed0e8bc05122ec8714590
Mick Thomure <thomure@cs.pdx.edu>
2012-05-02 15:15:35 -0700

    Clean up model test scripts.

--- glimpse/models/viz2/model_test.py
+++ glimpse/models/viz2/model_test.py
@@ -6,7 +6,7 @@
 import glimpse
 from glimpse.models.misc import DependencyError
 import Image
-from model import Model, State
+from model import Model, State, Layer
 import numpy as np
 from params import Params
 import unittest
@@ -24,8 +24,7 @@ class TestModel(unittest.TestCase):
     params.num_scales = self.NUM_SCALES
     params.s1_num_orientations = self.NUM_ORIENTATIONS
     model = Model(params = params)
-    L = model.Layer
-    if layer in (L.S2, L.C2, L.IT):
+    if layer in (Layer.S2, Layer.C2, Layer.IT):
       # Make uniform-random S2 kernels
       kernel_shape = (self.NUM_PROTOTYPES,) + model.s2_kernel_shape
       kernels = np.random.uniform(0, 1, size = kernel_shape)
@@ -62,11 +61,11 @@ class TestModel(unittest.TestCase):
     model = Model()
     state = State()
     def f():
-      return model.BuildLayer(Model.Layer.IMAGE, state)
+      return model.BuildLayer(Layer.IMAGE, state)
     self.assertRaises(DependencyError, f)
 
   def testBuildImageLayerFromPath(self):
-    layer = Model.Layer.IMAGE
+    layer = Layer.IMAGE
     model = self.MakeModel(layer)
     path = glimpse.GetExampleImagePaths()[0]
     state = model.MakeStateFromFilename(path)
@@ -76,41 +75,41 @@ class TestModel(unittest.TestCase):
     self.assertEqual(result.shape, img.size[::-1])
 
   def testBuildRetinaFromPath(self):
-    self.BuildLayerFromPath(Model.Layer.RETINA, np.ndarray)
+    self.BuildLayerFromPath(Layer.RETINA, np.ndarray)
 
   def testBuildRetinaFromInMemoryImage(self):
-    self.BuildLayerFromInMemoryImage(Model.Layer.RETINA, np.ndarray)
+    self.BuildLayerFromInMemoryImage(Layer.RETINA, np.ndarray)
 
   def testBuildS1(self):
-    result = self.BuildLayerFromPath(Model.Layer.S1, np.ndarray)
+    result = self.BuildLayerFromPath(Layer.S1, np.ndarray)
     self.assertEqual(result.ndim, 4)
     self.assertEqual(result.shape[:-2], (self.NUM_SCALES,
         self.NUM_ORIENTATIONS))
 
   def testBuildC1(self):
-    result = self.BuildLayerFromPath(Model.Layer.C1, np.ndarray)
+    result = self.BuildLayerFromPath(Layer.C1, np.ndarray)
     self.assertEqual(result.ndim, 4)
     self.assertEqual(result.shape[:-2], (self.NUM_SCALES,
         self.NUM_ORIENTATIONS))
 
   def testBuildS2(self):
-    result = self.BuildLayerFromPath(Model.Layer.S2, np.ndarray)
+    result = self.BuildLayerFromPath(Layer.S2, np.ndarray)
     self.assertEqual(result.ndim, 4)
     self.assertEqual(result.shape[:-2], (self.NUM_SCALES, self.NUM_PROTOTYPES))
 
   def testBuildC2(self):
-    result = self.BuildLayerFromPath(Model.Layer.C2, np.ndarray)
+    result = self.BuildLayerFromPath(Layer.C2, np.ndarray)
     self.assertEqual(result.ndim, 2)
     self.assertEqual(result.shape, (self.NUM_SCALES, self.NUM_PROTOTYPES))
 
   def testBuildIt(self):
-    result = self.BuildLayerFromPath(Model.Layer.IT, np.ndarray)
+    result = self.BuildLayerFromPath(Layer.IT, np.ndarray)
     self.assertEqual(result.ndim, 1)
     self.assertEqual(result.shape, (self.NUM_PROTOTYPES,))
 
   def testSampleC1Patches(self):
     num_patches = 10
-    model = self.MakeModel(Model.Layer.C1)
+    model = self.MakeModel(Layer.C1)
     path = glimpse.GetExampleImagePaths()[0]
     state = model.MakeStateFromFilename(path)
     result = model.SampleC1Patches(num_patches, state, normalize = True)
