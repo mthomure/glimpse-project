e4467e0ad9088b73c8b63532e12340481d0e3e64
Mick Thomure <mthomure@gmail.com>
2012-06-07 15:36:32 -0700

    Update to version 0.1.3.

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ cython_backend_ext = NumpyExtension(
 
 setup(
   name = "glimpse",
-  version = "0.1.2",
+  version = "0.1.3",
   author = "Mick Thomure",
   author_email = "thomure@cs.pdx.edu",
   packages = find_packages(),
