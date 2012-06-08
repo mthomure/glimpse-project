f2fc64689e56813ebfd4e9196dd173a0b7f2c597
Mick Thomure <mthomure@gmail.com>
2012-06-08 14:38:18 -0700

    Fix handling of cached functions in Gearman pool.

--- glimpse/pools/gearman_cluster/pool.py
+++ glimpse/pools/gearman_cluster/pool.py
@@ -98,6 +98,8 @@ class ClusterPool(gearman.GearmanClient):
   poll_timeout = 60.0
   #: Resubmit a job up to three times.
   max_retries = 3
+  #: Whether to send function objects via stateful data store by default.
+  cache_functions = False
 
   def __init__(self, host_list = None, command_url = None):
     """Create a new cluster pool.
@@ -110,27 +112,30 @@ class ClusterPool(gearman.GearmanClient):
     super(ClusterPool, self).__init__(host_list)
     self.command_url = command_url
 
-  def map(self, func, iterable, chunksize = None, cached_func = False):
+  def map(self, func, iterable, chunksize = None, cache_functions = None):
     """Apply a function to a list.
 
     :param func: Callable to evaluate.
     :param iterable: Input list.
     :param int chunksize: Number of arguments to pack into a single request.
-    :param bool cached_func: Whether the function should be stored in the
-    cluster's stateful data store (useful for large function objects).
+    :param bool cache_functions: Whether the function should be stored in the
+       cluster's stateful data store (useful for large function objects).
     :returns: Function output for each value in input list.
     :rtype: list
 
     """
     if chunksize == None:
       chunksize = self.chunksize
+    if cache_functions == None:
+      cache_functions = self.cache_functions
     # chunk states into groups
     request_groups = util.GroupIterator(iterable, chunksize)
     if func == None:
       raise ValueError("Function must not be empty.")
-    if cached_func:
-      self.SetMemory({CACHED_FUNC_KEY : func})
-      func = None
+    if cache_functions:
+      func_hash = hash(func)
+      self.SetMemory({func_hash : func})
+      func = func_hash
     # make tasks by combining each group with the transform
     batch_requests = [ dict(task = GEARMAN_TASK_MAP, data = data)
         for data in itertools.izip(itertools.repeat(func), request_groups) ]
@@ -153,8 +158,8 @@ class ClusterPool(gearman.GearmanClient):
           (len(failed_requests), len(completed),
           len(filter((lambda x: x.timed_out), job_requests)), buff))
     results = [ r.result for r in job_requests ]
-    if cached_func:
-      self.SetMemory({CACHED_FUNC_KEY : None})
+    if cache_functions:
+      self.SetMemory({func_hash : None})
     return list(util.UngroupIterator(results))
 
   #: Not implemented. This is currently an alias for :func:`map`.
@@ -188,6 +193,20 @@ def MakePool(config_file = None, chunksize = None):
   ``config_file`` (if specified), and the ``GLIMPSE_CLUSTER_CONFIG``
   environment variable (if set).
 
+  Example config file:
+
+     [client]
+     host = belmont.cs.pdx.edu
+     command_backend_url = tcp://%(host)s:9001
+     command_frontend_url = tcp://%(host)s:9000
+     log_backend_url = tcp://%(host)s:9003
+     log_frontend_url = tcp://%(host)s:9002
+     job_server_url = %(host)s:4730
+     cache_functions = True
+     chunksize = 8
+     poll_timeout = 60.0
+     max_retries = 3
+
   """
   config_files = list()
   if config_file != None:
@@ -202,8 +221,16 @@ def MakePool(config_file = None, chunksize = None):
   job_server_url = config.get('client', 'job_server_url')
   command_url = config.get('client', 'command_frontend_url')
   client = ClusterPool(host_list = [job_server_url], command_url = command_url)
+  if config.has_option('client', 'chunksize'):
+    chunksize = config.getint('client', 'chunksize')
   if chunksize != None:
     client.chunksize = chunksize
+  if config.has_option('client', 'poll_timeout'):
+    client.poll_timeout = config.getfloat('client', 'poll_timeout')
+  if config.has_option('client', 'max_retries'):
+    client.max_retries = config.getint('client', 'max_retries')
+  if config.has_option('client', 'cache_functions'):
+    client.cache_functions = config.getboolean('client', 'cache_functions')
   return client
 
 def CommandHandlerTarget(worker, cmd_future, log_future, ping_handler):
@@ -287,9 +314,11 @@ def RunWorker(job_server_url, command_url, log_url, num_processes = None):
     try:
       start = time.time()
       func, args = job.data
+      if type(func) == int:
+        logging.info("Looking up cached function for hash %d" % func)
+        func = worker.memory[func]
       if func == None:
-        logging.info("Looking up cached function")
-        func = worker.memory[CACHED_FUNC_KEY]
+        raise Exception("No function found")
       logging.info("Worker processing task with %d elements" % len(args))
       results = pool.map(func, args)
       elapsed_time = time.time() - start
