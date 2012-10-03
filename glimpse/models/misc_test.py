b115c55dc14b2411b6bab87dbe7ceeaabd0b17a2
Mick Thomure <mthomure@gmail.com>
2012-10-03 11:10:44 -0700

    Update unit tests.

--- glimpse/models/misc_test.py
+++ glimpse/models/misc_test.py
@@ -1,11 +1,12 @@
 
+import Image
 import os
 import unittest
 
 from glimpse import glab
 from glimpse.models.misc import LayerSpec, InputSource, \
-    InputSourceLoadException, BaseLayer, BaseState, BaseModel, \
-    DependencyError, ImprintS2Prototypes
+    InputSourceLoadException, BaseLayer, BaseState, BaseParams, BaseModel, \
+    DependencyError, ImprintKernels
 
 EXAMPLE_IMAGE = os.path.join(glab.GetExampleCorpus(), 'cats', 'Marcus_bed.jpg')
 EXAMPLE_IMAGE2 = os.path.join(glab.GetExampleCorpus(), 'dogs',
@@ -20,13 +21,13 @@ class Model(BaseModel):
 
   LayerClass = Layer
 
-  ParamClass = object
+  ParamClass = BaseParams
 
   StateClass = BaseState
 
   def _BuildSingleNode(self, output_id, state):
-    if output_id == Layer.RESHAPED_IMAGE.id:
-      data = state[Layer.IMAGE.id]
+    if output_id == Layer.RESHAPED_IMAGE.ident:
+      data = state[Layer.IMAGE.ident]
       return data.reshape((1,) + data.shape)
     return super(Model, self)._BuildSingleNode(output_id, state)
 
@@ -38,7 +39,7 @@ class S2Model(BaseModel):
 
   LayerClass = S2Layer
 
-  ParamClass = object
+  ParamClass = BaseParams
 
   StateClass = BaseState
 
@@ -47,8 +48,8 @@ class S2Model(BaseModel):
     return (2, 3)
 
   def _BuildSingleNode(self, output_id, state):
-    if output_id == S2Layer.C1.id:
-      data = state[S2Layer.IMAGE.id]
+    if output_id == S2Layer.C1.ident:
+      data = state[S2Layer.IMAGE]
       # Return the array with one scale and one band.
       return data.reshape((1, 1) + data.shape)
     return super(S2Model, self)._BuildSingleNode(output_id, state)
@@ -57,7 +58,7 @@ class TestBaseLayer(unittest.TestCase):
 
   def testFromId(self):
     lyr = Layer.RESHAPED_IMAGE
-    self.assertEqual(Layer.FromId(lyr.id), lyr)
+    self.assertEqual(Layer.FromId(lyr.ident), lyr)
 
   def testFromName(self):
     lyr = Layer.RESHAPED_IMAGE
@@ -101,27 +102,28 @@ class TestBaseModel(unittest.TestCase):
     # Model can't build source layer. This should raise an exception.
     model = Model()
     state = BaseState()
-    self.assertRaises(DependencyError, model._BuildSingleNode, Layer.SOURCE.id,
+    self.assertRaises(DependencyError, model._BuildSingleNode, Layer.SOURCE.ident,
         state)
 
   def testBuildNode(self):
     # Model should raise exception on missing dependencies.
     model = Model()
     state = BaseState()
-    self.assertRaises(DependencyError, model._BuildNode, Layer.SOURCE.id, state)
-    self.assertRaises(DependencyError, model._BuildNode, Layer.IMAGE.id, state)
+    self.assertRaises(DependencyError, model._BuildNode, Layer.SOURCE.ident,
+        state)
+    self.assertRaises(DependencyError, model._BuildNode, Layer.IMAGE.ident, state)
 
   def _testBuildLayer(self, use_callback):
     model = Model()
     state = model.MakeStateFromFilename(EXAMPLE_IMAGE)
-    layer = Layer.RESHAPED_IMAGE.id
+    layer = Layer.RESHAPED_IMAGE
     if use_callback:
       callback = model.BuildLayerCallback(layer)
       out_state = callback(state)
     else:
       out_state = model.BuildLayer(layer, state)
     self.assertNotEqual(out_state, None)
-    self.assertTrue(layer in out_state)
+    self.assertTrue(layer.ident in out_state)
     result = out_state[layer]
     self.assertNotEqual(result, None)
     self.assertEqual(result.shape, (1, 256, 256))
@@ -142,15 +144,15 @@ class TestBaseModel(unittest.TestCase):
   def testBuildLayer_failsOnBadImage(self):
     model = Model()
     state = model.MakeStateFromFilename(EXAMPLE_IMAGE_BAD)
-    layer = Layer.IMAGE.id
-    self.assertRaises(InputSourceLoadException, model.BuildLayer, layer, state)
+    self.assertRaises(InputSourceLoadException, model.BuildLayer, Layer.IMAGE,
+        state)
 
   def testBuildImageLayerFromPath(self):
     lyr = Layer.IMAGE
     model = Model()
     state = model.MakeStateFromFilename(EXAMPLE_IMAGE)
-    out_state = model.BuildLayer(model, lyr, state)
-    result = out_state[lyr.id]
+    out_state = model.BuildLayer(lyr, state)
+    result = out_state[lyr]
     img = Image.open(EXAMPLE_IMAGE)
     self.assertEqual(result.shape, img.size[::-1])
 
@@ -187,7 +189,8 @@ class TestImprint(unittest.TestCase):
     model = S2Model()
     states = map(model.MakeStateFromFilename, (EXAMPLE_IMAGE, EXAMPLE_IMAGE2))
     num_protos = 10
-    prototypes, locations = ImprintS2Prototypes(model, num_protos, states)
+    prototypes, locations = ImprintKernels(model, S2Layer.C1,
+        model.s2_kernel_sizes, num_protos, states)
     num_sizes = len(model.s2_kernel_sizes)
     self.assertEqual(len(prototypes), num_sizes)
     for protos_for_size, size in zip(prototypes, model.s2_kernel_sizes):
