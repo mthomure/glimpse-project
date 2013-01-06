7c59969a7d415e6e3bc8b817a9fb381250c9def5
Mick Thomure <mthomure@gmail.com>
2013-01-05 19:29:01 -0800

    Add convenience method to measure AUC of SVM.

--- glimpse/util/svm.py
+++ glimpse/util/svm.py
@@ -1,4 +1,9 @@
-"""This module provides access to the LIBSVM solver."""
+"""This module provides easy access to the LIBSVM solver.
+
+Glimpse stores feature vectors as a 3D array-like object. This module includes
+functions to convert this data to that required by scikit-learn.
+
+"""
 
 # Copyright (c) 2011 Mick Thomure
 # All rights reserved.
@@ -13,13 +18,14 @@ import sklearn.svm
 
 from glimpse import util
 
-def PrepareFeatures(features_per_class):
-  """Format feature vectors for use by the sklearn library.
+def GlimpseToSklearnLabels(features_per_class):
+  """Extract scikit-learn labels from Glimpse data.
 
   :param features_per_class: Per-class feature vectors.
-  :type features_per_class: 2D array-like
-  :returns: Feature vectors and labels.
-  :rtype: 2D-ndarray and 1D-ndarray
+  :type features_per_class: 3D array-like indexed by class, instance, and
+     feature.
+  :returns: Instance Labels.
+  :rtype: 1D-ndarray indexed by instance
 
   """
   num_classes = len(features_per_class)
@@ -30,11 +36,39 @@ def PrepareFeatures(features_per_class):
   class_sizes = map(len, features_per_class)
   labels_per_class = [ [label] * size
       for label, size in zip(labels, class_sizes) ]
-  features = util.UngroupLists(features_per_class)
-  features = np.array(features)
+  # Stack labels vertically. Result is 1D array.
   labels = util.UngroupLists(labels_per_class)
   labels = np.array(labels)
-  return features, labels
+  return labels
+
+def GlimpseToSklearnFeatures(features_per_class):
+  """Extract scikit-learn features from Glimpse data.
+
+  :param features_per_class: Per-class feature vectors.
+  :type features_per_class: 3D array-like indexed by class, instance, and
+     feature.
+  :returns: Feature vectors.
+  :rtype: 2D-ndarray indexed by instance and feature
+
+  """
+  # Stack features vertically. Result is 2D array-like.
+  features = util.UngroupLists(features_per_class)
+  features = np.array(features)
+  return features
+
+def PrepareFeatures(features_per_class):
+  """Format feature vectors for use by the sklearn library.
+
+  :param features_per_class: Per-class feature vectors.
+  :type features_per_class: 3D array-like indexed by class, instance, and
+     feature.
+  :returns: Feature vectors and labels.
+  :rtype: 2D-ndarray indexed by instance and feature, and 1D-ndarray indexed by
+     instance
+
+  """
+  return GlimpseToSklearnFeatures(features_per_class), \
+      GlimpseToSklearnLabels(features_per_class)
 
 class Pipeline(sklearn.pipeline.Pipeline):
   """A slightly customized sklearn pipeline, which implements the missing
@@ -56,7 +90,8 @@ def EasyTrain(train_features):
   """
   Train an SVM classifier.
 
-  Feature values are "sphered" by subtracting the mean and dividing by the standard deviation, as computed on the training set.
+  Feature values are "sphered" by subtracting the mean and dividing by the
+  standard deviation, as computed on the training set.
 
   :param train_features: Training data indexed by class, instance, and features.
   :type train_features: 3D array-like
@@ -64,13 +99,13 @@ def EasyTrain(train_features):
 
   """
   # Prepare the data
-  features, labels = PrepareFeatures(train_features)
+  labels = GlimpseToSklearnLabels(train_features)
+  features = GlimpseToSklearnFeatures(train_features)
   # Create the SVM classifier with feature scaling.
   classifier = Pipeline([ ('scaler', sklearn.preprocessing.Scaler()),
                           ('svm', sklearn.svm.LinearSVC())])
   classifier.fit(features, labels)
   return classifier
-
 
 def EasyTest(classifier, test_features):
   """
@@ -85,11 +120,32 @@ def EasyTest(classifier, test_features):
 
   """
   # Prepare the data
-  features, labels = PrepareFeatures(test_features)
+  labels = GlimpseToSklearnLabels(test_features)
+  features = GlimpseToSklearnFeatures(test_features)
   # Evaluate the classifier
   predicted_labels = classifier.predict(features)
   accuracy = sklearn.metrics.zero_one_score(labels, predicted_labels)
   return accuracy
+
+def EasyTestAUC(classifier, test_features):
+  """
+  Apply a built classifier to a set of data.
+
+  :param classifier: A trained classifier, as returned by :func:EasyTrain:.
+  :type classifier: sklearn.base.ClassifierMixin
+  :param test_features: Testing data indexed by class, instance, and feature.
+  :type test_features: 3D array-like
+  :rtype: float
+  :return: Area under the ROC curve of classifier on test data.
+
+  """
+  # Prepare the data
+  labels = GlimpseToSklearnLabels(test_features)
+  features = GlimpseToSklearnFeatures(test_features)
+  # Evaluate the classifier
+  dvalues = classifier.decision_function(features)
+  auc = sklearn.metrics.auc_score(labels, dvalues[:, 0])
+  return auc
 
 def EasyCrossVal(classifier, features, num_folds = 5):
   """
